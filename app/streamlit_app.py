26e4487d72a1c88303223526fbf9d453365f934a
AUDERGO <audrey.vernet@volvo.com>
2026-06-02 13:53:14 +0200

    Create streamlit_app.py

---
+++ app/streamlit_app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+from src.matching import compute_matrix
+
+st.title("Matching Ergonomique")
+
+cotation_file = st.file_uploader("Cotation", type=["xlsx"])
+restriction_file = st.file_uploader("Restriction", type=["xlsx"])
+
+if cotation_file and restriction_file:
+
+    cotation = pd.read_excel(cotation_file)
+    restriction = pd.read_excel(restriction_file)
+
+    result = compute_matrix(cotation, restriction)
+
+    st.write("### Matrice de matching")
+    st.dataframe(result)
+
+    st.download_button(
+        "Télécharger Excel",
+        result.to_csv(index=False),
+        "matrice.csv"
+    )
